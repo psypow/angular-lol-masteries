3e28f2459dc3bec5408cc487cf218746f9934ce0
psypow <psypow.dev@gmail.com>
2015-09-20 21:02:01 +0200

    try again to fix travis grunt

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function (grunt) {
+    console.log("GRUNTFILE");
     grunt.loadNpmTasks('grunt-karma');
 
     grunt.initConfig({
         karma:{
-
             unit: {
                 configFile: 'karma.conf.js',
                 port: 9999,
                 singleRun: true,
-                browsers: ['Chrome'],
-                logLevel: 'ERROR'
+                browsers: ['Phantom'],
+                logLevel: 'INFO'
             }
         }
     });
