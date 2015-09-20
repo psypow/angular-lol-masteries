d2888e3390e1cb954772240a3582a2315fd45caa
psypow <psypow.dev@gmail.com>
2015-09-20 03:03:14 +0200

    added jsdoc in masteriesData

--- src/services/masteriesData.js
+++ src/services/masteriesData.js
@@ -3,6 +3,11 @@ module.exports = function(ngModule){
         var masteriesDataUrlTemplate = 'http://ddragon.leagueoflegends.com/cdn/$version$/data/$language$/mastery.json';
         var forEach = angular.forEach;
 
+        /**
+         *
+         * @param options [Object] language and version
+         * @returns {Function|promise}
+         */
         function getMasteriesStaticData(options) {
             var defer = $q.defer(),
                 language = (options && options.language ) || 'en_US',
@@ -23,6 +28,11 @@ module.exports = function(ngModule){
             return defer.promise;
         }
 
+        /**
+         *
+         * @param masteries {Array}
+         * @returns {Array}
+         */
         function convertStaticMasteriesToPlainArray(masteries){
             var plainMasteriesArray = [];
             forEach(masteries.tree, function(masteriesBlock){
