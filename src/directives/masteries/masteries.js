bf12d01a9737f0eee8374da9a27f84f417a1b1ee
psypow <psypow.dev@gmail.com>
2015-09-20 03:03:43 +0200

    masteries ng-repeat with position classes

--- src/directives/masteries/masteries.js
+++ src/directives/masteries/masteries.js
@@ -2,7 +2,27 @@ module.exports = function(ngModule){
     ngModule.directive('masteries', ['masteriesData', function(masteriesData){
         return {
             replace: true,
-            template: require('./masteries.tpl.html')
+            template: require('./masteries.tpl.html'),
+            controller:function($scope){
+                const masteryClass='mastery',
+                    masteryRow='--row',
+                    masteryColumn='--column',
+                    masteryBlock='--block';
+
+                masteriesData.getMasteriesStaticData().then(function(masteries){
+                    $scope.masterySlots = masteriesData.convertStaticMasteriesToPlainArray(masteries);
+                });
+
+                $scope.getPositionClasses = (mastery) => {
+                    var masteryId = mastery && mastery.masteryId;
+
+                    return masteryId?{
+                      [masteryClass + masteryBlock + masteryId[1]]:true,
+                      [masteryClass + masteryRow + masteryId[2]]:true,
+                      [masteryClass + masteryColumn + masteryId[3]]:true
+                    }:'';
+                }
+            }
         };
     }]);
 };
